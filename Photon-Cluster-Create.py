24118e3ab68639a32f6ecbee57424bd15ddcaa7c
davlloyd <david.lloyd@emc.com>
2016-08-25 15:56:50 +1000

    Added new file to track curl commands

--- Photon-Cluster-Create.py
+++ Photon-Cluster-Create.py
@@ -6,7 +6,7 @@
 ##############################################################################
 import requests
 from requests.auth import HTTPDigestAuth
-import json
+import json, menu
 
 # Header formation for resftul Posts Puts, etc
 headers = {'Content-Type': 'application/json'}
@@ -17,7 +17,6 @@ apiPort = '9000'
 
 # Tenancy focus for location of cluster to be created
 myTenant = input("Enter Tenant Name: ")
-myProject = input("Enter Tenant Project: ")
 
 # Will add logic for user login later in case Lightwave enabled for authentication
 # myResponse = requests.get(url,auth=HTTPDigestAuth(input("User Name: "), input("Password: ")), verify=True)
@@ -31,7 +30,10 @@ if(myResponse.ok):
 
     # Get the deployment ID to target the cluster creation
     tenantId = resData['items'][0]['id']
-    print(tenantId)
+
+    myResponse = requests.get('http://%s:%s/tenants/%s/projects' % (hostAddress, apiPort, tenantid), verify=True)
+    print(myResponse)
+    #myProject = input("Enter Tenant Project: ")
      
 else:
     myResponse.raise_for_status()
