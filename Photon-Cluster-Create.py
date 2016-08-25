64c9f89c06d7ae62ea950edb910668f536fcfc4e
davlloyd <david.lloyd@emc.com>
2016-08-25 11:09:32 +1000

    Added script for Host addition

--- Photon-Cluster-Create.py
+++ Photon-Cluster-Create.py
@@ -8,25 +8,30 @@ import requests
 from requests.auth import HTTPDigestAuth
 import json
 
-#Address to connect to the Controll instance or northbound NLB service
-url = "http://10.63.251.31:9000/deployments"
+# Header formation for resftul Posts Puts, etc
+headers = {'Content-Type': 'application/json'}
+
+# Address to connect to the Controll instance or northbound NLB service
+hostAddress = "10.63.251.31"
+apiPort = '9000'
 
 # Tenancy focus for location of cluster to be created
-myTenant = raw_input("Enter Tenant Name: ")
-myProject = raw_input("Enter Tenant Project: ")
+myTenant = input("Enter Tenant Name: ")
+myProject = input("Enter Tenant Project: ")
 
 # Will add logic for user login later in case Lightwave enabled for authentication
-# myResponse = requests.get(url,auth=HTTPDigestAuth(raw_input("User Name: "), raw_input("Password: ")), verify=True)
+# myResponse = requests.get(url,auth=HTTPDigestAuth(input("User Name: "), input("Password: ")), verify=True)
 
-myResponse = requests.get(url, verify=True)
+myResponse = requests.get('http://%s:%s/tenants?name=%s' % (hostAddress, apiPort, myTenant), verify=True)
 
 if(myResponse.ok):
+    print("Tenant Found")
+    # Read the response to the deployment object
+    resData = json.loads(myResponse.text)
 
-    jData = json.loads(myResponse.content)
-
-    print("The response contains {0} properties".format(len(jData)))
-    print("\n")
-    for key in jData:
-        print(key + " : " + jData[key])
+    # Get the deployment ID to target the cluster creation
+    tenantId = resData['items'][0]['id']
+    print(tenantId)
+     
 else:
     myResponse.raise_for_status()
---
+++ Photon-Host-Add.py
@@ -0,0 +1,36 @@
+##############################################################################
+# Platform:     Photon Controller 1.0RC1
+# Description:  Host Add Script
+# Notes:        Written for Python 3.x   
+# Log:          
+##############################################################################
+import requests
+from requests.auth import HTTPDigestAuth
+import json
+
+# Header formation for resftul Posts Puts, etc
+headers = {'Content-Type': 'application/json'}
+
+# Address to connect to the Controll instance or northbound NLB service
+hostAddress = "10.63.251.31"
+apiPort = '9000'
+
+# Will add logic for user login later in case Lightwave enabled for authentication
+# myResponse = requests.get(url,auth=HTTPDigestAuth(input("User Name: "), input("Password: ")), verify=True)
+
+myResponse = requests.get('http://%s:%s/deployments' % (hostAddress, apiPort), verify=True)
+
+if(myResponse.ok):
+    # Read the response to the deployment object
+    resData = json.loads(myResponse.text)
+
+    # Get the deployment ID to target the cluster creation
+    deployId = resData['items'][0]['id']
+    print(deployId)
+     
+
+    # Add hostAddress
+
+    # Set Host to paused 
+else:
+    myResponse.raise_for_status()
