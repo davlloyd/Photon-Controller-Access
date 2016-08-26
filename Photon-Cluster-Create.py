9037f3f0f9105d72edb8fd350c6d320103ddd203
davlloyd <david.lloyd@emc.com>
2016-08-27 06:56:21 +1000

    Update to cluster provisioning script

--- Photon-Cluster-Create.py
+++ Photon-Cluster-Create.py
@@ -16,7 +16,10 @@ headers = {'Content-Type': 'application/json'}
 # Address to connect to the Controll instance or northbound NLB service
 hostAddress = "10.63.251.31"
 apiPort = '9000'
+
+# General cluster variables
 clusterFile = 'mesosclustertemplate.json'
+clusterName = 'notthename'
 
 # Get the Id for the Tenant specified by the user
 def getTenantID():
@@ -79,20 +82,23 @@ def getProjectID(tenantId):
 
 # Create the cluster into the select project
 def createCluster(projectId):
-    global hostAddress, apiPort, clusterFile
+    global hostAddress, apiPort, clusterFile, clusterName
 
     if os.path.isfile(clusterFile):
         clusterTemplate = open(clusterFile, 'r')
         content = clusterTemplate.read()
         clusterTemplate.close()
 
-        content = content.replace('<NAME>', input("\nEnter Cluster Name: "))
+        clusterName = input("\nEnter Cluster Name: ")
+
+        content = content.replace('<NAME>', clusterName)
         content = content.replace('<NETWORKID>', getNetworkId())
         content = content.replace('<ZOO1>', input("\nEnter Zookeeper IPAddress: "))
         content = content.replace('<NETMASK>', "255.255.255.0")
         content = content.replace('<GATEWAY>', "10.63.251.1")
-    
-        aResponse = requests.post('http://%s:%s/projects/%s/clusters' % (hostAddress, apiPort, projectId), json=json.loads(content), verify=True)
+
+        print("\nCluster Creation Starting")    
+        aResponse = requests.post('http://%s:%s/projects/%s/clusters' % (hostAddress, apiPort, projectId), json=json.loads(content))
         if(aResponse.ok):
             resData = json.loads(aResponse.text)
             try:
@@ -132,29 +138,94 @@ def getNetworkId():
 
     
 # Get the UI IP address for cluster
-def clusterConsoleIp(clusterId):
-    return None
+# Only need the project id sent through as cluster name is set as global value 
+def clusterConsoleIp(projectId):
+    global hostAddress, apiPort
+    global clusterName
+
+    # Get all the networks
+    myResponse = requests.get('http://%s:%s/projects/%s/clusters' % (hostAddress, apiPort, projectId), verify=True)
+    
+    #make sure it responded well
+    if(myResponse.ok):
+        # get the clusters listed
+        for clusterItem in json.loads(myResponse.text)['items']:
+            if(clusterItem['name'] == clusterName):
+                clusterId = clusterItem['id']
+                break
+        
+        if(clusterId):
+            clusterVMs = requests.get('http://%s:%s/clusters/%s/vms' % (hostAddress, apiPort, clusterId), verify=True)
+            if(clusterVMs.ok):
+                # Getting the VMs from the cluster and looking for the VM with the Marathon role
+                for vmItem in json.loads(clusterVMs.text)['items']:
+                    if(vmItem['name'].upper().startswith("MARATHON")):
+                        vmIp = getVMIP(vmItem['id'])
+                        return vmIp
+            else:
+                print("Failed to get VM list from cluster %s" % clusterName)
+                return None
+        else:
+            print("Did not find cluster %s" % clusterName)
+            return None
+    else:
+        print('Reponse from Photon failed')
+        return None
+
+# Get tghe IP address of a vm
+def getVMIP(vmId):
+    global hostAddress, apiPort
+
+    # Get all the networks associated to a vm, this creates a task to collect the address details from the 
+    # vm. The VM Network object will return task identifier which collects the details
+    resData = requests.get('http://%s:%s/vms/%s/networks' % (hostAddress, apiPort, vmId), verify=True)
+        
+    #make sure it responded well
+    if(resData.ok):
+        try:
+            # Now need to get the task id as the network data is reported within the task as it is a point in 
+            # time value due to DHCP so is not stored with the vm
+            taskId = json.loads(resData.text)['id']
+            resData = requests.get('http://%s:%s/tasks/%s' % (hostAddress, apiPort, taskId), verify=True)
+            # Reading the network interface to find one with an id. The private container has a null id
+            # Of course this might change
+            for netConn in json.loads(resData.text)['resourceProperties']['networkConnections']:
+                if netConn['network']:
+                    ipAddress = netConn['ipAddress']
+
+            return ipAddress
+        except:
+            print("Failed getting task")
+            return None
+    
 
 # Wait for tasks to complete
 def taskWait(taskId):
     global hostAddress, apiPort
 
-    print("Tracking Task: ")
+    print("\nTracking Task: -", end='')
+    counter = 0
     while True:
+        counter += 1
         # Get the task details
         aResponse = requests.get('http://%s:%s/tasks/%s' % (hostAddress, apiPort, taskId), verify=True)
         
         #make sure it responded well
         if(aResponse.ok):
+            if counter % 2 == 0:
+                print('\b|', end='')
+            else:
+                print('\b-', end='')
+
             resData = json.loads(aResponse.text)
             try:
                 taskState = resData['state']
                 if taskState == "COMPLETED":
                     return True
                 elif taskState == "ERROR":
-                    print("Task did not complete successfully")
+                    print("\nTask did not complete successfully")
                     return None
-                print("\r.")
+
                 time.sleep(5)
             except:
                 print("\nTask status tracking failed")
@@ -170,10 +241,13 @@ if(tenantId):
     projectId = getProjectID(tenantId)
     taskId = createCluster(projectId)
     if(taskWait(taskId)):
-        clusterId = clusterConsoleIp()
-        if (clusterId):
-            print('Cluster Created, browse to http://%s to access Marathon' % clusterId)
+        # Get the cluster UI address
+        clusterConsoleIp = clusterConsoleIp(projectId)
+        if (clusterConsoleIp):
+            print('\nCluster %s Created, browse to http://%s:8080 to access Marathon' % (clusterName, clusterConsoleIp))
         else:
-            print('Oops... something didn''t work!')   
+            print('\nOops... something didn''t work getting the cluster details!')
+    else:
+        print("\nSomething happened waiting for the task to complete") 
 else:
-    print("Tenant not found")
+    print("\nTenant not found")
